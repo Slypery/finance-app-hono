af74eabb04431637cf57b6fa49bda5a67f06d033
Slypery <ibnuumar634@gmail.com>
2026-04-14 14:03:32 +0700

    changing how db is connected

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,10 @@
 import { drizzle } from 'drizzle-orm/bun-sql'
+import { SQL } from 'bun'
 import * as schema from '@/db/schema'
 import { DATABASE_URL } from '@/env'
 
-export const db = drizzle({ connection: DATABASE_URL, schema: schema })
+const g = global as typeof globalThis & { sql?: SQL }
+
+g.sql ??= new SQL(DATABASE_URL)
+
+export const db = drizzle(g.sql, { schema })
